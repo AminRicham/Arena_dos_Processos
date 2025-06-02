cfaa0c20111f3b44fe36d499f02ccf851980c52c
Thiago Oliveira <113373282+ThiagoS1lva@users.noreply.github.com>
2025-06-02 11:58:24 -0300

    initial commit

--- robots.py
+++ robots.py
@@ -1,13 +1,21 @@
 from random import randint
 
-class Robot:
-    def __init__(self, ID, F, E, V, posicao, status):
-        self.ID = ID
-        self.F = F
-        self.E = E
-        self.V = V
-        self.posicao = posicao
-        self.status = status
+# Constantes para pegar do grid
+# Testes
+LARGURA_GRID = 20 
+ALTURA_GRID = 20
 
-    #TODO funções do robo - Pessoa 2
+
+class Robot:
+    def __init__(self, ID, F, E, V, posicao_x, posicao_y, status):
+        self.ID = ID
+        self.forca = F
+        self.energia = E
+        self.velocidade = V
+        self.posicao_x = posicao_x
+        self.posicao_y = posicao_y
+        self.status = status # Vivo ou morto
+
+    
+
     

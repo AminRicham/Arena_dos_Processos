6b1bc306dee6fb7bbbc95639fa6299f87feb4b74
Thiago Oliveira <113373282+ThiagoS1lva@users.noreply.github.com>
2025-06-02 15:06:03 -0300

    update - Movimentação do robo

--- robots.py
+++ robots.py
@@ -21,10 +21,11 @@ class Robot:
         self.posicao_x = posicao_x
         self.posicao_y = posicao_y
         self.status = status # Vivo ou morto
+        self.log = [] # Log de ações do robô
         
         # Adiciona o robô na grid se a posição estiver vazia
-        with grid_mutex:
-            GRID[self.y][self.x] = str(self.id)
+        with grid_mutex: # Protegendo o grid
+            GRID[self.posicao_x][self.posicao_y] = str(self.ID)
 
     def mover(self):
         if self.energia <= 0:
@@ -36,14 +37,29 @@ class Robot:
         dx *= self.velocidade
         dy *= self.velocidade
         # Definindo nova posição
-        nova_posicao_x = max(0, min(LARGURA_GRID - 1, self.posicao_x + dx))
+        nova_posicao_x = max(0, min(LARGURA_GRID - 1, self.posicao_x + dx)) # Verificação para não sair do grid e chegar no ultimo bloco
         nova_posicao_y = max(0, min(ALTURA_GRID - 1, self.posicao_y + dy))
-        print(f"Robo {self.ID} movendo de ({self.posicao_x}, {self.posicao_y}) para ({nova_posicao_x}, {nova_posicao_y})")
         
-        
+        with grid_mutex:
+            if GRID[nova_posicao_x][nova_posicao_y] == "-":
+                # Atualiza a posição do robô na grid
+                GRID[self.posicao_x][self.posicao_y] = "-" # Limpa a posição antiga
+                GRID[nova_posicao_x][nova_posicao_y] = str(self.ID) # Define a nova posição do robô
+                self.log.append(f"Robo {self.ID} movendo de ({self.posicao_x}, {self.posicao_y}) para ({nova_posicao_x}, {nova_posicao_y})")
+                self.posicao_x, self.posicao_y = nova_posicao_x, nova_posicao_y
+                self.energia -= 1
+                self.log.append(f"{vars(self)}")
+            else:
+                self.log.append(f"Robo {self.ID} tentou se mover para ({nova_posicao_x}, {nova_posicao_y}), mas a posição já está ocupada.")
+    
+    def mostrar_log(self):
+        print(f"Log do Robô {self.ID}:")
+        for acao in self.log:
+            print(acao)
 
 # testar a classe Robot
-robo1 = Robot(ID=1, F=10, E=100, V=5, posicao_x=random.randint(0, 20), posicao_y=random.randint(0, 20), status="Vivo")
+robo1 = Robot(ID=1, F=10, E=100, V=5, posicao_x=random.randint(0, 19), posicao_y=random.randint(0, 19), status="Vivo")
 robo1.mover()
+robo1.mostrar_log()
 #print(vars(robo1))
     

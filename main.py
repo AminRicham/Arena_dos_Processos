01a2c38550ba7a06bd36f6fec73eba1941f874c8
AminRicham <aminricham@gmail.com>
2025-06-09 10:17:49 -0300

    Update - Memoria compartilhada

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from multiprocessing import Lock, Array
 import shared_struct as ss
 import grid as gdd
+
 def main():
+
     grid = Array(typecode_or_type='u', size_or_initializer=ss.GRID_SIZE, lock=True)
-    #robots = Array(typecode_or_type='i', size_or_initializer=(7*qtdRobos), lock=True)
-    #flags = Array(typecode_or_type= "i", size_or_initializer=qtdFlags, lock=True)
+    grid_mutex = Lock()
+
+    robots = Array(typecode_or_type='i', size_or_initializer=(5*qtdRobos), lock=True)
+    robots_mutex = Lock()
+
+    flags = Array(typecode_or_type= "i", size_or_initializer=qtdFlags, lock=True)
+    flags_mutex = Lock()
+
+    battery_mutex = Lock()
+
     gdd.iniciaGrid(grid)
     gdd.adicionaElementos(grid)
     gdd.printGrid(grid)

2ee3ab6db682ae83cb376de8b616c9cbf60f37dd
AminRicham <aminricham@gmail.com>
2025-06-09 13:53:18 -0300

    Update - main

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from multiprocessing import Lock, Array
 import shared_struct as ss
-import grid as gdd
+import gridFunctions
+import flagsFunctions
 
 def main():
-
     grid = Array(typecode_or_type='u', size_or_initializer=ss.GRID_SIZE, lock=True)
     grid_mutex = Lock()
 
-    robots = Array(typecode_or_type='i', size_or_initializer=(5*qtdRobos), lock=True)
+    robots = Array(typecode_or_type='i', size_or_initializer=(5*ss.QTD_ROBOS), lock=True)
     robots_mutex = Lock()
 
-    flags = Array(typecode_or_type= "i", size_or_initializer=qtdFlags, lock=True)
+    flags = Array(typecode_or_type= "i", size_or_initializer=(ss.QTD_FLAGS), lock=True)
     flags_mutex = Lock()
 
-    battery_mutex = Lock()
+    battery_mutex = [Lock() for _ in range (ss.QTD_BATERIAS)]
 
-    gdd.iniciaGrid(grid)
-    gdd.adicionaElementos(grid)
-    gdd.printGrid(grid)
+    gridFunctions.iniciaGrid(grid)
+    flagsFunctions.initFlags(flags)
+
 
 if __name__ == '__main__':
     main()
